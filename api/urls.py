86304065d96d699922885c51bf0b3dcb5f927a69
Bogdan Iftoda <bogdaniftoda@gmail.com>
2021-11-06 18:37:32 +0200

    s

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BoquetInfoList, ItemList, OrderDetailsList, OrderList, PhotosList, UsersList, RegisterView, ItemDetail
+from .views import ItemList, OrderDetailsList, OrderList, PhotosList, UsersList, RegisterView, ItemDetail
 
 
 urlpatterns = [
@@ -8,7 +8,6 @@ urlpatterns = [
     path('item_detail/<int:item_id>/', ItemDetail.as_view()),
     path('photo_list/', PhotosList.as_view()),
     path('order_list/', OrderList.as_view()),
-    path('boquet_info_list/', BoquetInfoList.as_view()),
     path('order_details_list/', OrderDetailsList.as_view()),
     path('register/', RegisterView.as_view(), name='auth_register'),
 ]
