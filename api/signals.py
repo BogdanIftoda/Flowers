5b861d41a036a9acc493193c3a13a922beeb476b
Bogdan Iftoda <bogdaniftoda@gmail.com>
2021-11-07 18:44:47 +0200

    migrate db

--- api/signals.py
+++ api/signals.py
@@ -1,8 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import User
-from rest_framework_simplejwt.models import TokenUser
+from rest_framework_simplejwt.tokens import Token
 
 @receiver(post_save, sender=User)
 def notify_task_save(sender, instance, **kwargs):
-    TokenUser.objects.create(user=instance)
+    Token.objects.create(user=instance)
