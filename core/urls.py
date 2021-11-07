4ad970de1571e9f97185df4c469cf9787a98b886
Bogdan Iftoda <bogdaniftoda@gmail.com>
2021-11-07 19:35:52 +0200

    migrate db

--- core/urls.py
+++ core/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from rest_framework_simplejwt import views as jwt_views
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -23,12 +23,16 @@ from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_swagger_view(title='Flowers API')
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/', include('api.urls')),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', schema_view),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
