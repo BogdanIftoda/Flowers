acead153323404402aaba56649225b5c11fd19b0
Bogdan Iftoda <bogdaniftoda@gmail.com>
2021-11-07 18:40:40 +0200

    migrate db

--- api/admin.py
+++ api/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 
 from .models import Item, Order, OrderDetails, Photo, User
-from rest_framework_simplejwt.models import TokenUser
 
-# Register your models here.
-admin.site.register(TokenUser)
 admin.site.register(User)
 admin.site.register(Item)
 admin.site.register(Order)
