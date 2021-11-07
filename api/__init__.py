8b8330bf2bb543d6faa731f20b3adb8d3e21a721
Bogdan Iftoda <bogdaniftoda@gmail.com>
2021-11-07 18:48:24 +0200

    migrate db

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,1 @@
-from django.contrib.auth import default_app_config
-
-
 default_app_config = 'api.apps.ApiConfig'
--- api/apps.py
+++ api/apps.py
@@ -3,6 +3,6 @@ from django.apps import AppConfig
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'
+    # name = 'api'
     def ready(self) -> None:
         import api.signals
