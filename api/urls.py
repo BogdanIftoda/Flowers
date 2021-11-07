93e38000bc6d9d3d66c71b3470f0e82bb440d798
Bogdan Iftoda <bogdaniftoda@gmail.com>
2021-11-07 15:07:30 +0200

    viewSet for items

--- api/urls.py
+++ api/urls.py
@@ -1,33 +1,33 @@
-from django.urls import path, include
-from .views import ItemList, OrderDetailsList, OrderList, PhotosList, UsersList, RegisterView, ItemDetail, UserViewSet, CurrentUser
-
-
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
+from .views import (CurrentUser, OrderDetailsList, OrderList, PhotosList,
+                    RegisterView, UserViewSet, ItemViewSet)
+
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
+router.register(r'items', ItemViewSet)
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('register/', RegisterView.as_view(), name='auth_register'),
     path('current_user/', CurrentUser.as_view()),
-    path('user_list/', UsersList.as_view()),
-    path('item_list/', ItemList.as_view()),
-    path('item_detail/<int:item_id>/', ItemDetail.as_view()),
     path('photo_list/', PhotosList.as_view()),
     path('order_list/', OrderList.as_view()),
     path('order_details_list/', OrderDetailsList.as_view()),
-    path('register/', RegisterView.as_view(), name='auth_register'),
+
 ]
 
 
 # from rest_framework.permissions import IsAuthenticated
 # from rest_framework.authentication import TokenAuthentication
 
-# # Product Generic Viewset 
+# # Product Generic Viewset
 # class ProductViewSet(viewsets.ModelViewSet):
 #     queryset = Product.objects.all()
 #     serializer_class = ProductSerializer
-        
+
 #     authentication_classes = [TokenAuthentication]
 #     permission_classes = [IsAuthenticated]
 
--- api/views.py
+++ api/views.py
@@ -5,11 +5,9 @@ from .models import Item, Order, OrderDetails, Photo, User
 from .serializers import (ItemSerializer,
                           OrderDetailsSerializer, OrderSerializer,
                           PhotoSerializer, UserSerializer, RegisterSerializer)
-from django.http import Http404
 from rest_framework.response import Response
 
 from .permissions import IsAuthorOrReadOnly
-
 
 
 from rest_framework import viewsets
@@ -17,10 +15,11 @@ from rest_framework import viewsets
 
 class UserViewSet(viewsets.ModelViewSet):
     """
-    This viewset automatically provides `list` and `retrieve` actions.
+    This viewset provides `get`,`post`, `put`, `patch`, `delete` methods.
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
 
 class RegisterView(CreateAPIView):
     """
@@ -31,27 +30,23 @@ class RegisterView(CreateAPIView):
     serializer_class = RegisterSerializer
 
 
-class ItemDetail(APIView):
-    """
-        Item detail view
-    """
-    def get_object(self, item_id):
-        try:
-            return Item.objects.get(id=item_id)
-        except Item.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, item_id):
-        item = self.get_object(item_id)
-        serializer = ItemSerializer(item)
-        return Response(serializer.data)
-
-
 class CurrentUser(APIView):
+    """
+        Get current user
+    """
 
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+
+
+class ItemViewSet(viewsets.ModelViewSet):
+    """
+    This viewset provides `get`,`post`, `put`, `patch`, `delete` methods.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
 
 class UsersList(ListAPIView):
     queryset = User.objects.all()
@@ -71,7 +66,7 @@ class PhotosList(ListAPIView):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
     # permission_classes = (
-        # permissions.IsAuthenticated,)
+    # permissions.IsAuthenticated,)
 
 
 class OrderList(ListAPIView):
@@ -80,10 +75,9 @@ class OrderList(ListAPIView):
     permission_classes = (
         permissions.IsAuthenticated,)
 
+
 class OrderDetailsList(ListAPIView):
     queryset = OrderDetails.objects.all()
     serializer_class = OrderDetailsSerializer
     permission_classes = (
         permissions.IsAuthenticated,)
-
-
