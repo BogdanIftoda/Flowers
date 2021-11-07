038082a93c6a822effe009d47a469e2145b6f16e
Bogdan Iftoda <bogdaniftoda@gmail.com>
2021-11-07 19:45:20 +0200

    migrate db

--- core/settings.py
+++ core/settings.py
@@ -153,6 +153,7 @@ AUTH_USER_MODEL = 'api.User'
 #DRF config
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
