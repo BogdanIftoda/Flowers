5b7a5226f2780d5a3be62f13fd122e889fa35b1f
Bogdan Iftoda <bogdaniftoda@gmail.com>
2021-11-22 21:33:26 +0200

    pagination

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import permissions, viewsets
 from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
@@ -46,6 +47,8 @@ class ItemViewSet(viewsets.ModelViewSet):
     """
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    pagination_class = PageNumberPagination
+
 
 
 class PhotosList(ListAPIView):
