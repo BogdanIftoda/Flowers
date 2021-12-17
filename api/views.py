a2d8af88ea43826fede7f0fcc2c5991391d7221d
Bogdan Iftoda <bogdaniftoda@gmail.com>
2021-12-17 17:17:54 +0200

    modify swagger

--- api/views.py
+++ api/views.py
@@ -1,70 +1,90 @@
 from rest_framework import permissions, viewsets
 from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, \
+    DestroyModelMixin
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
 
 from .models import Item, Order, OrderDetails, Photo, User
 from .serializers import (ItemSerializer, OrderDetailsSerializer,
                           OrderSerializer, PhotoSerializer, RegisterSerializer,
                           UserSerializer)
+from drf_yasg import openapi
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    This viewset provides `get`,`post`, `put`, `patch`, `delete` methods.
-    """
+current_user = openapi.Response('', UserSerializer)
+
+
+@method_decorator(name='update', decorator=swagger_auto_schema(request_body=UserSerializer))
+@method_decorator(name='partial_update', decorator=swagger_auto_schema(request_body=UserSerializer))
+class UserViewSet(GenericViewSet, CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (permissions.IsAuthenticated,)
-    authentication_classes = [JWTAuthentication, ]
+    # permission_classes = (permissions.IsAuthenticated, )
+    authentication_classes = (JWTAuthentication, )
 
 
+@method_decorator(name='create', decorator=swagger_auto_schema(request_body=RegisterSerializer))
 class RegisterView(CreateAPIView):
     """
         Registration view
     """
+
     queryset = User.objects.all()
     permission_classes = (permissions.AllowAny,)
     serializer_class = RegisterSerializer
+
+
+
 
 
 class CurrentUser(APIView):
     """
         Get current user
     """
+
     authentication_classes = [JWTAuthentication, ]
 
+    @swagger_auto_schema(tags=['current user'], responses={200: current_user})
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
 
 
 class ItemViewSet(viewsets.ModelViewSet):
-    """
-    This viewset provides `get`,`post`, `put`, `patch`, `delete` methods.
-    """
+
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
     permissions_class = PageNumberPagination
 
+
 class PhotosList(ListAPIView):
+    """
+         Get current user
+     """
+
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
 
+    @swagger_auto_schema(tags=['photo list'])
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
 
 class OrderViewSet(viewsets.ModelViewSet):
-    """
-    This viewset provides `get`,`post`, `put`, `patch`, `delete` methods.
-    """
+
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
 
 class OrderDetailsViewSet(viewsets.ModelViewSet):
-    """
-    This viewset provides `get`,`post`, `put`, `patch`, `delete` methods.
-    """
+
     queryset = OrderDetails.objects.all()
     serializer_class = OrderDetailsSerializer
--- core/urls.py
+++ core/urls.py
@@ -19,9 +19,18 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from rest_framework_swagger.views import get_swagger_view
+# from rest_framework_swagger.views import get_swagger_view
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+# schema_view = get_swagger_view(title='Flowers API')
 
-schema_view = get_swagger_view(title='Flowers API')
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Flowers API",
+        default_version='1.0.0',
+    ),
+    public=True
+)
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -34,5 +43,5 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('', schema_view),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
