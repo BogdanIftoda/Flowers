9eae3f23218d4b18ea3495bd9afef222813635cf
Bogdan Iftoda <bogdaniftoda@gmail.com>
2021-12-26 17:41:24 +0200

    delete permission

--- api/views.py
+++ api/views.py
@@ -29,7 +29,6 @@ class UserViewSet(GenericViewSet, CreateModelMixin, ListModelMixin, RetrieveMode
                   DestroyModelMixin):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser)
     authentication_classes = (JWTAuthentication,)
 
 
@@ -62,7 +61,6 @@ class CurrentUser(APIView):
 class ItemViewSet(GenericViewSet, CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin,
                   DestroyModelMixin):
     queryset = Item.objects.all()
-    # permission_classes = (permissions.IsAuthenticated, )
 
     def get_serializer_class(self):
         if self.action in ['create', 'update', 'partial_update']:
@@ -89,48 +87,50 @@ class OrderViewSet(GenericViewSet, CreateModelMixin, ListModelMixin, RetrieveMod
                   DestroyModelMixin):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    # permission_classes = (permissions.IsAuthenticated, )
 
     def get_serializer_class(self):
-        if self.action in ['create', 'update', 'partial_update']:
+        if self.action == 'create':
             return OrderSerializer
         return OrderUserSerializer
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    # def perform_create(self, serializer):
+    #     serializer.save(user=self.request.user)
 
     def create(self, request, *args, **kwargs):
+        data = request.data
+        order_serializer = OrderSerializer(data=data)
+        order_serializer.is_valid(raise_exception=True)
         user = request.user
-        print(request.data)
-        items = kwargs['order_detail_items']
-        items_list = []
+        items = data['order_detail_items']
+        print(items)
         for pk in items:
-            items = get_object_or_404(Item, id=pk)
-        # ordered_product_qs = OrderDetails.objects.filter(
-        #     item=item)
-        # if ordered_product_qs.exists():
-        #     ordered_product = ordered_product_qs[0]
-        #     ordered_product.quantity += 1
-        #     ordered_product.save()
-        #     return Response({'success': 'item quantity updated'}, status=status.HTTP_200_OK)
-        # else:
-        #     order_qs = Order.objects.filter(user=user)
-        #     ordered_product = OrderDetails.objects.create(
-        #         user=user, item=item)
+            item = get_object_or_404(Item, id=pk)
+            print(item)
+            ordered_product_qs = OrderDetails.objects.filter(
+                item=item)
+            # print(ordered_product_qs)
+            if ordered_product_qs.exists():
+                ordered_product = ordered_product_qs[0]
+                ordered_product.quantity += 1
+                ordered_product.save()
+            else:
+                order_qs = Order.objects.filter(user=user, created=False)
+                print(order_qs)
+                ordered_product = OrderDetails.objects.create(
+                    user=user, item=item)
 
-        #     if order_qs.exists():
-        #         order = order_qs[0]
-        #         order.order_detail_items.add(ordered_product)
-        #         order.save()
-        #         return Response({'success': 'item added to your cart'}, status=status.HTTP_200_OK)
+                if order_qs.exists():
+                    order = order_qs[0]
+                    order.order_detail_items.add(ordered_product)
+                    order.save()
 
-        #     else:
-        #         order = Order.objects.create(user=user)
-        #         order.order_detail_items.add(ordered_product)
-        #         order.save()
-        #         return Response({'success': 'item added to your cart'}, status=status.HTTP_200_OK)
+                else:
+                    order = Order.objects.create(user=user)
+                    order.delivery_address = data['delivery_address']
+                    order.order_detail_items.add(ordered_product)
+                    order.save()
 
-        return Response()
+        return Response(status=status.HTTP_200_OK)
 
 @method_decorator(name='update', decorator=swagger_auto_schema(request_body=OrderDetailsSerializer))
 @method_decorator(name='partial_update', decorator=swagger_auto_schema(request_body=OrderDetailsSerializer))
@@ -150,35 +150,3 @@ class OrderDetailsViewSet(GenericViewSet, CreateModelMixin, ListModelMixin, Retr
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
-
-class AddToCartAPIView(GenericAPIView):
-
-    def get(self, request, pk):
-        user = request.user
-        print(user)
-        item = get_object_or_404(Item, id=pk)
-        ordered_product_qs = OrderDetails.objects.filter(
-            item=item)
-        if ordered_product_qs.exists():
-            ordered_product = ordered_product_qs[0]
-            ordered_product.quantity += 1
-            ordered_product.save()
-            return Response({'success': 'item quantity updated'}, status=status.HTTP_200_OK)
-        else:
-            order_qs = Order.objects.filter(user=user)
-            ordered_product = OrderDetails.objects.create(
-                user=user, item=item)
-
-            if order_qs.exists():
-                order = order_qs[0]
-                order.order_detail_items.add(ordered_product)
-                order.save()
-                return Response({'success': 'item added to your cart'}, status=status.HTTP_200_OK)
-
-            else:
-                order = Order.objects.create(user=user)
-                order.order_detail_items.add(ordered_product)
-                order.save()
-                return Response({'success': 'item added to your cart'}, status=status.HTTP_200_OK)
-
